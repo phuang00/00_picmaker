268e4606179a2b8533bfdb9f583d55658c9bdc68
phuang00 <phuang00@stuy.edu>
2020-01-31 20:05:52 -0500

    finished a pic

--- picmaker.c
+++ picmaker.c
@@ -4,22 +4,34 @@
 #include <unistd.h>
 
 int main() {
-  int i, j, fd;
-  fd = open("image.ppm", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-  char buffer[20];
-  int rgb[3];
-  snprintf(buffer, sizeof(buffer), "P3 600 600 255\n");
-  write(fd, buffer, sizeof(buffer));
-  for (i = 0; i < 600; i++){
-    for (j = 0; j < 600; j++){
-      rgb[0] = i % 256;
-      rgb[1] = j % 256;
-      snprintf(buffer, sizeof(buffer), "%d %d %d\n", rgb[0], rgb[1], 0);
-      printf("%s\n", buffer);
-      write(fd, buffer, sizeof(buffer));
+  int i, j;
+  FILE * fp;
+  fp = fopen("image.ppm", "w");
+  fprintf(fp, "P3\n768 768\n255\n");
+  for (i = 0; i < 768; i++){
+    for (j = 0; j < 768; j++){
+      if (i < 128){
+        fprintf(fp, "%d %d %d\n", 255, 0, i * 2);
+      }
+      else if (i < 256){
+        fprintf(fp, "%d %d %d\n", 511 - i * 2, 0, 255);
+      }
+      else if (i < 384){
+        fprintf(fp, "%d %d %d\n", 0, (i * 2), 255);
+      }
+      else if (i < 512){
+        fprintf(fp, "%d %d %d\n", 0, 255, 1023 - i * 2);
+      }
+      else if (i < 640){
+        fprintf(fp, "%d %d %d\n", i * 2 % 256, 255, 0);
+      }
+      else{
+        fprintf(fp, "%d %d %d\n", 255, 1535 - i * 2, 0);
+        printf("%d\n", 1535 - i * 2);
+      }
     }
   }
-  close(fd);
+  fclose(fp);
   /* code */
   return 0;
 }
